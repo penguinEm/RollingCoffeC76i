fadcff9479ffd99f1e8c88d17921db23dffce330
emir paradi <emir.alejandro.paradi@gmail.com>
2024-02-06 17:46:22 -0300

    Se agregaron los NavLink y Link de la libreria de react routes con sus respectivas rutas

--- src/components/common/BarraDeNavegacion.jsx
+++ src/components/common/BarraDeNavegacion.jsx
@@ -1,24 +1,27 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import logo from "../../assets/Coffee_Logo.png";
+import { Link, NavLink } from "react-router-dom";
 
 const BarraDeNavegacion = () => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <img
-          src={logo}
-          alt="Logo de la marca"
-          className="img-fluid logoDimensiones py-2"
-        />
+        <Navbar.Brand as={Link} to={"/"}>
+          <img
+            src={logo}
+            alt="Logo de la marca"
+            className="img-fluid logoDimensiones py-2"
+          />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="#home" active>
+            <NavLink end className="nav-link" to={"/"}>
               Inicio
-            </Nav.Link>
-            <Nav.Link href="#link">Administrador</Nav.Link>
-            <Nav.Link href="#link">Login</Nav.Link>
-            <Nav.Link href="#link">Registro</Nav.Link>
+            </NavLink>
+            <NavLink end className="nav-link" to="/administrador">Administrador</NavLink>
+            <NavLink end className="nav-link" to="/login">Login</NavLink>
+            <NavLink end className="nav-link" to="*">Registro</NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
