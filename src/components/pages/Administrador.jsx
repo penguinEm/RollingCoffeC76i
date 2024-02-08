5c5ec6f1171e0318c7b40a283802f30797b73578
emir paradi <emir.alejandro.paradi@gmail.com>
2024-02-08 17:27:41 -0300

    Traemos los productos de la api en el montaje de Administrador

--- src/components/pages/Administrador.jsx
+++ src/components/pages/Administrador.jsx
@@ -1,9 +1,27 @@
 import { Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import ContenedorTabla from "./ContenedorTabla";
-
+import { useState, useEffect } from "react";
+import { leerProductosApi } from "../../helpers/queries";
 
 const Administrador = () => {
+  /* Variables globales---------------- */
+  const [porductos, setProductos] = useState([]);
+
+  /* Funciones ------------------------ */
+  useEffect(() => {
+    traerProductos();
+  }, []);
+
+  const traerProductos = async() => {
+    try {
+      await leerProductosApi();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  /* Logica extra y ,maquetado */
   return (
     <Container className="main ">
       <section className="d-flex align-items-center accordion-collapsemt-5 border-bottom pb-3">
