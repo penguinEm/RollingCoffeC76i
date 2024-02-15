bb624973eeddda7ea7d59047ccdcdc529b65fd5c
emir paradi <emir.alejandro.paradi@gmail.com>
2024-02-15 02:24:54 -0300

    Traemos los productos de la api en el montaje del inicio

--- src/components/pages/ContenedorCards.jsx
+++ src/components/pages/ContenedorCards.jsx
@@ -1,7 +1,26 @@
 import { Container } from "react-bootstrap";
-import Producto from "./producto/Producto";
+import Producto from "../pages/producto/Producto";
+import { leerProductosApi } from "../../helpers/queries";
+import { useState, useEffect } from "react";
 
 const ContenedorCards = () => {
+   /* Variables globlaes---------------------------------- */
+   const [productos, setProductos] = useState([]);
+
+   /* Funciones ------------------------------------------ */
+   useEffect(() => {
+     traerProductos();
+   }, []);
+ 
+   const traerProductos = async () => {
+     try {
+       await leerProductosApi();
+     } catch {
+       console.log(error);
+     }
+   };
+ 
+   /* Logica extra y maquetado --------------------------- */
   return (
     <Container>
       <h1 className=" display-4 mt-5 border-bottom pb-3">Nuestros Productos</h1>
